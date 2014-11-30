385769830b2c51aec611cc3b55c46254d034228a
andres-root <andres.root.coder@gmail.com>
2014-11-30 14:15:14 -0500

    working on firefox

--- stream/main.js
+++ stream/main.js
@@ -3,5 +3,8 @@ var streamToAttach
 
 params = {audio: true, video: true}
 navigator.getUserMedia( params,  function(stream) {
-	
+	video.src = window.URL.createObjectUrl(stream);
+	streamToAttach = stream;
+}, function(error) {
+	alert(erro)
 });
